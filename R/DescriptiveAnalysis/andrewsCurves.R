0665a6f56bf7bc35266ee15c4691acfd9c04a5c8
Jose Antonio Perusquia <j.perusquia.c@gmail.com>
2026-02-16 09:39:02 -0600

    Update

--- R/DescriptiveAnalysis/andrewsCurves.R
+++ R/DescriptiveAnalysis/andrewsCurves.R
@@ -1,88 +1,79 @@
-############################################################################
-# Título:
-#     Curvas de Andrews
-#
-# Autor: 
-#     José A. Perusquía Cortés
-#
-# Descripción: 
-#     Código para generar las curvas de Andrews como fueron propuestas
-#     en Andrews, D.F. (1972). Plots of High-Dimensional Data. Biometrics,
-#     28(1), 125–136.
-############################################################################
+###################################################################
+# Source code for the Andrews curves as proposed by Andrews (1972)
+# Author: Jose Antonio Perusquia Cortes
+# Afil : Facultad de Ciencias - UNAM
+# Module : Multivariate Analysis
+###################################################################
 
-############################################################################
-# Librerias
-library(ggplot2)
-############################################################################
+###################################################################
+# Required libraries
+library(ggplot2)          # Version 3.5.2
+library(ggthemes)         # Version 5.1.0
+###################################################################
 
-andrewsCurves=function(x,groups,xlab="",ylab="",title="",
-                       legend.title=element_blank(),
-                       is.grouped=T){
+###################################################################
+# Source code for the original implementation of the Andrews 
+# Curves. The function receives as parameters:
+# x = the matrix of observations
+# groups = the labels for each observation
+# xlab, ylab, title, legend.title for the plot
+
+andrewsCurves <- function(x, groups = NULL,
+                          xlab = "", ylab = "", title = "",
+                          legend.title = NULL) {
   
-  t=seq(-pi,pi,length.out=200)
-  n=nrow(x)
-  p=ncol(x)
+  x <- as.matrix(x)
   
-  f=matrix(0,length(t),p)
-  f[,1]=1/sqrt(2)
+  t <- seq(-pi, pi, length.out = 200)
+  n <- nrow(x)
+  p <- ncol(x)
+  
+  # Build Fourier basis
+  f <- matrix(0, length(t), p)
+  f[,1] <- 1/sqrt(2)
   
   for(i in 2:p){
-    if(i%%2==0){
-      f[,i]=sin(floor(i/2)*t)
-    }else{
-      f[,i]=cos(floor(i/2)*t)
+    if(i %% 2 == 0){
+      f[,i] <- sin((i/2)*t)
+    } else {
+      f[,i] <- cos(((i-1)/2)*t)
     }
   }
   
-  res=matrix(0,n,length(t))
-
-  res[1,]=apply(t(f)*x[1,],2,sum)
+  # Vectorized transformation
+  res <- x %*% t(f)
   
-  if(is.grouped){
-    df=data.frame(x=t,y=res[1,],col=groups[1])
-    
-    p=ggplot(df)+
-      geom_line(aes(x,y,colour=col))
-    
-    for(i in 2:n){
-      res[i,]=apply(t(f)*x[i,],2,sum)
-      df=data.frame(x=t,y=res[i,],col=groups[i])
-      p=p+geom_line(data=df,mapping=aes(x,y,colour=col))
-    }
-    
-    p=p+
-      theme_minimal()+
-      xlab(xlab)+
-      ylab(ylab)+
-      labs(title=title)+
-      theme(
-        legend.title = legend.title
-      )
-    plot(p)
-    
-  }else{
-    df=data.frame(x=t,y=res[1,])
-    
-    p=ggplot(df)+
-      geom_line(aes(x,y))
-    
-    for(i in 2:n){
-      res[i,]=apply(t(f)*x[i,],2,sum)
-      df=data.frame(x=t,y=res[i,])
-      p=p+geom_line(data=df,mapping=aes(x,y))
-    }
-    
-    p=p+
-      theme_minimal()+
-      xlab(xlab)+
-      ylab(ylab)+
-      labs(title=title)+
-      theme(
-        legend.position='none'
-      )
-    plot(p)
+  # Long format
+  df <- data.frame(
+    t = rep(t, each = n),
+    value = as.vector(res),
+    id = rep(1:n, times = length(t))
+  )
+  
+  if(!is.null(groups)){
+    df$group <- rep(groups, times = length(t))
   }
+  
+  # Base plot
+  if(!is.null(groups)){
+    p <- ggplot(df, aes(t, value, group = id, colour = group)) +
+      geom_line()
+    
+    # Apply legend title if provided
+    if(!is.null(legend.title)){
+      p <- p + labs(colour = legend.title)
+    }
+    
+  } else {
+    p <- ggplot(df, aes(t, value, group = id)) +
+      geom_line() +
+      theme(legend.position = "none")
+  }
+  
+  p <- p +
+    theme_minimal() +
+    labs(x = xlab, y = ylab, title = title)
+  
+  return(p)
 }
-
-
+###################################################################
